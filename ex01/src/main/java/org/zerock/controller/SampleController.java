7b0b04148d8990f247a8bfdfb8367b4683a3281e
YoungQWER <wkdrnr321@naver.com>
2024-04-09 13:17:54 +0900

    second

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -35,13 +35,6 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SampleController {
 
-	/*
-	 * @InitBinder public void initBinder(WebDataBinder binder) { SimpleDateFormat
-	 * dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	 * binder.registerCustomEditor(java.util.Date.class, new
-	 * CustomDateEditor(dateFormat, false)); }
-	 */
-
 	@RequestMapping("")
 	public void basic() {
 		log.info("basic................");
